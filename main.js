ea8564dcb326edddb5724aabbd89ddc76d418517
Urszula <u.postolowicz@gmail.com>
2021-02-02 10:32:06 +0100

    Create basic "add" function

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ let result = 0;
 let operation = "";
 let numbers = [];
 
+const operations = () => {
+  if (operation === "add") {
+    for (i = 0; i < numbers.length; i++) {
+      result += numbers[i];
+    }
+    return result;
+  }
+};
+
 const checkButtonType = (e) => {
   if (e.target.classList.contains("digit")) {
     const number = Number(e.target.textContent);
@@ -20,10 +29,19 @@ const checkButtonType = (e) => {
     operation = "";
   }
   if (e.target.classList.contains("addition")) {
+    numbers.push(Number(screen.textContent));
+    if (operation === "result") {
+      numbers = [];
+    }
     operation = "add";
+    screen.textContent = 0;
   }
   if (e.target.classList.contains("equal")) {
+    if (screen.textContent !== "0") numbers.push(Number(screen.textContent));
+    operations();
     console.log(numbers);
+    screen.textContent = result;
+    operation = "result";
   }
 };
 
