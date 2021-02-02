2a46b517319e1dd832beae2eda5b131ad52cb226
Urszula <u.postolowicz@gmail.com>
2021-02-02 10:14:23 +0100

    Type numbers which contains more than one digit

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const buttons = [...document.querySelectorAll(".button")];
 const screen = document.querySelector(".screen");
 
+let result = 0;
+let operation = "";
+let numbers = [];
+
 const checkButtonType = (e) => {
   if (e.target.classList.contains("digit")) {
     const number = Number(e.target.textContent);
-    screen.textContent = number;
+    if (screen.textContent === "0") {
+      screen.textContent = number;
+    } else {
+      screen.textContent += number;
+    }
   }
   if (e.target.classList.contains("reset")) {
     screen.textContent = "0";
+    numbers = [];
+    operation = "";
+  }
+  if (e.target.classList.contains("addition")) {
+    operation = "add";
+  }
+  if (e.target.classList.contains("equal")) {
+    console.log(numbers);
   }
 };
 
